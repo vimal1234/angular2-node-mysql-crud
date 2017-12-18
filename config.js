0f5ba95c4075f8a09a2e316c31d0f910dce70753
shariefinnodevtechnologiescom <sharief@innodevtechnologies.com>
2017-12-18 18:14:25 +0530

    Second commit

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {
 	database: {
-		host:	  'sql12.freesqldatabase.com', 	// database host
-		user: 	  'sql12211355', 		// your database username
-		password: 'yDYqRuqql4', 		// your database password
+		host:	  'localhost', 	// database host
+		user: 	  'root', 		// your database username
+		password: '', 		// your database password
 		port: 	  3306, 		// default MySQL port
-		db: 	  'sql12211355' 		// your database name
+		db: 	  'employee' 		// your database name
 	},
 	server: {
 		host: '127.0.0.1',
